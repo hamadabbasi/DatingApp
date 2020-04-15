5e6bee75c9f6bb98fd6be3983bcacb826cd4f519
unknown <hamad.abbasi@hotmail.com>
2020-04-15 05:06:23 +0300

    comiting login changes

--- DatingApp.api/Startup.cs
+++ DatingApp.api/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DatingApp.api.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,6 +30,7 @@ namespace DatingApp.api
             services.AddDbContext<Data.DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
